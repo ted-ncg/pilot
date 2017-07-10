b8322673b1e31013e715dad7856cd92507225ede
Ted M. Young <tedyoung@gmail.com>
2017-07-10 07:02:46 -0500

    Project initialized by Spring Initialzr

---
+++ src/main/java/com/visa/ncg/canteen/CanteenApplication.java
@@ -0,0 +1,12 @@
+package com.visa.ncg.canteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanteenApplication.class, args);
+	}
+}
