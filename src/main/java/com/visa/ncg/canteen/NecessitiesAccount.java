01eee48557fbb8ba424cdf2058e20f156b4e911f
Ted M. Young <tedyoung@gmail.com>
2017-07-10 15:43:33 -0500

    Deposit no longer allows amounts <= 0.

--- src/main/java/com/visa/ncg/canteen/NecessitiesAccount.java
+++ src/main/java/com/visa/ncg/canteen/NecessitiesAccount.java
@@ -4,6 +4,9 @@ public class NecessitiesAccount {
   private int balance;
 
   public void deposit(int amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Deposits of negative or 0 dollars is not allowed.");
+    }
     balance = balance + amount;
   }
 
--- src/test/java/com/visa/ncg/canteen/NecessitiesAccountTest.java
+++ src/test/java/com/visa/ncg/canteen/NecessitiesAccountTest.java
@@ -3,8 +3,20 @@ package com.visa.ncg.canteen;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NecessitiesAccountTest {
+
+
+  @Test
+  public void depositZeroOrNegativeDollarsToAccountResultsInExceptionThrown() {
+    NecessitiesAccount necessitiesAccount = new NecessitiesAccount();
+    assertThatThrownBy(() -> necessitiesAccount.deposit(0))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Deposits of negative or 0 dollars is not allowed.");
+    assertThatThrownBy(() -> necessitiesAccount.deposit(-1))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
 
   @Test
   public void aNewAccountWithDepositOf10DollarsResultsInAccountHaving10DollarBalance() {
